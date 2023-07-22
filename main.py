235d0a49768334ebdbbeda97b8c6ee5bb8d077f4
Swagatika-ai <swagatika.som123@gmail.com>
2023-07-23 05:11:17 +0530

    data ingestion & prepare base model done

---
+++ main.py
@@ -0,0 +1,26 @@
+from CNNClassifer import logger
+from CNNClassifer.pipeline.phase_01_data_ingestion import DataIngestionTrainingPipeline
+from CNNClassifer.pipeline.phase_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Phase"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Prepare Base Model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
